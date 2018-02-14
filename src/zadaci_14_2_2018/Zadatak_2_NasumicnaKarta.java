efa2dda8bf8a40b3e7c64cc01a7508f3e3d04aeb
Omer Salkanovic <omko_s_99@hotmail.com>
2018-02-14 15:19:48 +0100

    Uredjena zadaca_14_2_2018

---
+++ src/zadaci_14_2_2018/Zadatak_2_NasumicnaKarta.java
@@ -0,0 +1,12 @@
+package zadaci_14_2_2018;
+
+public class Zadatak_2_NasumicnaKarta extends Methods {
+
+	public static void main(String[] args) {
+		runApp();
+	}
+
+	public static void runApp() {
+		baciKartu();
+	}
+}
